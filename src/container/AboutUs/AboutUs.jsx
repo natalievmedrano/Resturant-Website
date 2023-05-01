29051f13c27abf15f2b9c34622a1ef26c35e7d32
natalievmedrano <nataliemed0403@gmail.com>
2023-05-01 17:21:35 -0500

    fixed minor margin erros

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -9,38 +9,45 @@ const AboutUs = () => (
     id="about"
   >
     <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="G" />
+      <img src={images.G} alt="letter g" />
     </div>
-    <div className="aboutus">
-      <h1 className="headtext__cormorant">About Us</h1>
-      <img src={images.spoon} alt="spoon" className="spoonimage" />
-      <p className="p__opensans">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <button type="button" className="custom__button">Know More</button>
-    </div>
-    <div className="knifeimg">
-      <img src={images.knife} alt="knife" />
-    </div>
-    <div className="history">
-      <h1 className="headtext__cormorant">Our History</h1>
-      <img src={images.spoon} alt="spoon" className="spoonimage" />
-      <p className="p__opensans">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <button type="button" className="custom__button">Know More</button>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">ABOUT US</h1>
+        <img src={images.spoon} alt="about_spoon" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knifeimg" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
     </div>
   </div>
 );
