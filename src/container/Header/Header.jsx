9c3f7fd7526ab839ae6d026b813cfe475c337bc1
natalievmedrano <nataliemed0403@gmail.com>
2023-05-01 15:58:56 -0500

    added image to header + styled text and image

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
     <SubHeading title={"Chase The New Flavor"} />
     <h1 className="app__header-h1">The Key to Fine Dining</h1>
     <p className="p__opensans" style={{ margin: "2rem 0" }}>
-      LOREM IPSUM GENERATOR Lorem ipsum dolor sit amet, consectetur adipiscing
+      Lorem ipsum dolor sit amet, consectetur adipiscing
       elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
       aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
@@ -22,9 +22,9 @@ const Header = () => (
       EXPLORE MENU
     </button>
     </div>
-    
+
     <div className="app__wrapper_img">
-      {/* <img src={images.welcome} alt="img" /> */}
+      <img src={images.welcome} alt="img" />
     </div>
   </div>
 );
