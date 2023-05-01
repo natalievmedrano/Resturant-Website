44f31ca72310fe861a3184687c3840df86f14da7
natalievmedrano <nataliemed0403@gmail.com>
2023-05-01 18:01:19 -0500

    working on menu section mapped over wine to display on menu section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,8 +7,8 @@ import "./Header.css";
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-    <SubHeading title={"Chase The New Flavor"} />
-    <h1 className="app__header-h1">The Key to Fine Dining</h1>
+    <SubHeading title={"DISCOVER THE KEY TO FINE DINING"} />
+    <h1 className="app__header-h1">New York's Finest</h1>
     <p className="p__opensans" style={{ margin: "2rem 0" }}>
       Lorem ipsum dolor sit amet, consectetur adipiscing
       elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './SpecialMenu.css';
+import { SubHeading, MenuItem } from "../../components";
+import { images, data } from "../../constants";
+import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialmenu-title">
+      <SubHeading title={"Menu that fits your pallete"} />
+      <h1 className="headtext__cormorant">Todays Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu-menu_items">
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu" />
+      </div>
+      <div></div>
+    </div>
   </div>
 );
 
