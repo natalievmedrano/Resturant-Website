a64d1c1a188d96492b1c60962596b2541bfa020b
natalievmedrano <nataliemed0403@gmail.com>
2023-05-01 15:53:12 -0500

    created divs and class names for headers now working on styling the header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+    <SubHeading title={"Chase The New Flavor"} />
+    <h1 className="app__header-h1">The Key to Fine Dining</h1>
+    <p className="p__opensans" style={{ margin: "2rem 0" }}>
+      LOREM IPSUM GENERATOR Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+    </p>
+    <button type="button" className="custom__button">
+      EXPLORE MENU
+    </button>
+    </div>
+    
+    <div className="app__wrapper_img">
+      {/* <img src={images.welcome} alt="img" /> */}
+    </div>
   </div>
 );
 
