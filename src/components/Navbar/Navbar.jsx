bff6f097c0aa63aa2e2f6d5bc3c141f8954f30d6
natalievmedrano <nataliemed0403@gmail.com>
2023-05-01 14:58:16 -0500

    installed react icons + working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import{MdOutlineResturantMenu} from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className="navbarlogo">
+      <img src={images.gericht} alt="logo" />
+    </div>
+    <ul className='app_nav-links'>
+      <li className='p_opensans'> <a href="#home">Home</a></li>
+      <li className='p_opensans'> <a href="#about">About</a></li>
+      <li className='p_opensans'> <a href="#menu">Menu</a></li>
+      <li className='p_opensans'> <a href="#awards">Awards</a></li>
+      <li className='p_opensans'> <a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
